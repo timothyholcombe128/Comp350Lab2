eb789709897147d6013a06046e812e900f3bd7b8
Timothy Holcombe <timothy.holcombe128@myci.csuci.edu>
2015-09-02 13:49:26 -0700

    first commit

---
+++ MVCDemo(1)/MVCDemo.java
@@ -0,0 +1,29 @@
+package mvcdemo;
+
+public class MVCDemo {
+
+  public static void main(String[] args) {
+
+      
+      Student model  = retriveStudentFromDatabase();
+
+      StudentView view = new StudentView();
+
+      StudentController controller = new StudentController(model, view);
+
+      controller.updateView();
+
+      controller.setStudentName("John");
+
+      controller.updateView();
+   }
+
+   private static Student retriveStudentFromDatabase(){
+      Student student = new Student();
+      student.setName("Robert");
+      student.setRollNo("10");
+      return student;
+   }
+    
+}
+
